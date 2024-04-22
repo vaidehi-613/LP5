1c1a9e5fc7276d4840fbdcbb3b6cf94b1c09eb21
vaidehi-613 <89066994+vaidehi-613@users.noreply.github.com>
2024-04-22 21:49:14 +0530

    Update 2_parallel_bubble.cpp

--- HPC/prac2/2_parallel_bubble.cpp
+++ HPC/prac2/2_parallel_bubble.cpp
@@ -1,70 +1,61 @@
 #include<iostream>
+#include<stdlib.h>
 #include<omp.h>
-
 using namespace std;
 
-void bubble(int array[], int n){
-    for (int i = 0; i < n - 1; i++){
-        for (int j = 0; j < n - i - 1; j++){
-            if (array[j] > array[j + 1]) swap(array[j], array[j + 1]);
-        }
-    }
+void bubble(int *, int);
+void swap(int &, int &);
+
+
+void bubble(int *a, int n)
+{
+    for(  int i = 0;  i < n;  i++ )
+     {  	 
+   	 int first = i % 2; 	 
+
+   	 #pragma omp parallel for shared(a,first)
+   	 for(  int j = first;  j < n-1;  j += 2  )
+   	   {  	 
+   		 if(  a[ j ]  >  a[ j+1 ]  )
+   		  {  	 
+     			 swap(  a[ j ],  a[ j+1 ]  );
+   		  }  	 
+   		   }  	 
+     }
 }
 
-void pBubble(int array[], int n){
-    //Sort odd indexed numbers
-    for(int i = 0; i < n; ++i){    
-        #pragma omp for
-        for (int j = 1; j < n; j += 2){
-        if (array[j] < array[j-1])
-        {
-          swap(array[j], array[j - 1]);
-        }
-    }
 
-    // Synchronize
-    #pragma omp barrier
+void swap(int &a, int &b)
+{
 
-    //Sort even indexed numbers
-    #pragma omp for
-    for (int j = 2; j < n; j += 2){
-      if (array[j] < array[j-1])
-      {
-        swap(array[j], array[j - 1]);
-      }
-    }
-  }
+    int test;
+    test=a;
+    a=b;
+    b=test;
+
 }
 
-void printArray(int arr[], int n){
-    for(int i = 0; i < n; i++) cout << arr[i] << " ";
-    cout << "\n";
+int main()
+{
+
+    int *a,n;
+    cout<<"\n enter total no of elements=>";
+    cin>>n;
+    a=new int[n];
+    cout<<"\n enter elements=>";
+    for(int i=0;i<n;i++)
+    {
+   	 cin>>a[i];
+    }
+    
+    bubble(a,n);
+    
+    cout<<"\n sorted array is=>";
+    for(int i=0;i<n;i++)
+    {
+   	 cout<<a[i]<<endl;
+    }
+
+
+return 0;
 }
-
-int main(){
-    // Set up variables
-    int n = 10;
-    int arr[n];
-    int brr[n];
-    double start_time, end_time;
-
-    // Create an array with numbers starting from n to 1
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    // Sequential time
-    start_time = omp_get_wtime();
-    bubble(arr, n);
-    end_time = omp_get_wtime();     
-    cout << "Sequential Bubble Sort took : " << end_time - start_time << " seconds.\n";
-    printArray(arr, n);
-    
-    // Reset the array
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    // Parallel time
-    start_time = omp_get_wtime();
-    pBubble(arr, n);
-    end_time = omp_get_wtime();     
-    cout << "Parallel Bubble Sort took : " << end_time - start_time << " seconds.\n";
-    printArray(arr, n);
-}   
