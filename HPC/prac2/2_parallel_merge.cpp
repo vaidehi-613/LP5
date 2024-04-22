0e0039e8a414d07e5b0f23df830923fd003e8825
vaidehi-613 <89066994+vaidehi-613@users.noreply.github.com>
2024-04-22 21:48:46 +0530

    Update 2_parallel_merge.cpp

--- HPC/prac2/2_parallel_merge.cpp
+++ HPC/prac2/2_parallel_merge.cpp
@@ -1,102 +1,96 @@
-#include <iostream>
-#include <omp.h>
-
+#include<iostream>
+#include<stdlib.h>
+#include<omp.h>
 using namespace std;
 
-void merge(int arr[], int low, int mid, int high) {
-    // Create arrays of left and right partititons
-    int n1 = mid - low + 1;
-    int n2 = high - mid;
 
-    int left[n1];
-    int right[n2];
+void mergesort(int a[],int i,int j);
+void merge(int a[],int i1,int j1,int i2,int j2);
+
+void mergesort(int a[],int i,int j)
+{
+	int mid;
+	if(i<j)
+	{
+    	mid=(i+j)/2;
+   	 
+    	#pragma omp parallel sections
+    	{
+
+        	#pragma omp section
+        	{
+            	mergesort(a,i,mid);   	 
+        	}
+
+        	#pragma omp section
+        	{
+            	mergesort(a,mid+1,j);    
+        	}
+    	}
+
+    	merge(a,i,mid,mid+1,j);    
+	}
+
+}
+ 
+void merge(int a[],int i1,int j1,int i2,int j2)
+{
+	int temp[1000];    
+	int i,j,k;
+	i=i1;    
+	j=i2;    
+	k=0;
     
-    // Copy all left elements
-    for (int i = 0; i < n1; i++) left[i] = arr[low + i];
+	while(i<=j1 && j<=j2)    
+	{
+    	if(a[i]<a[j])
+    	{
+        	temp[k++]=a[i++];
+    	}
+    	else
+    	{
+        	temp[k++]=a[j++];
+    }    
+	}
     
-    // Copy all right elements
-    for (int j = 0; j < n2; j++) right[j] = arr[mid + 1 + j];
-    
-    // Compare and place elements
-    int i = 0, j = 0, k = low;
-
-    while (i < n1 && j < n2) {
-        if (left[i] <= right[j]){
-            arr[k] = left[i];
-            i++;
-        } 
-        else{
-            arr[k] = right[j];
-            j++;
-        }
-        k++;
-    }
-
-    // If any elements are left out
-    while (i < n1) {
-        arr[k] = left[i];
-        i++;
-        k++;
-    }
-
-    while (j < n2) {
-        arr[k] = right[j];
-        j++;
-        k++;
-    }
+	while(i<=j1)    
+	{
+    	temp[k++]=a[i++];
+	}
+   	 
+	while(j<=j2)    
+	{
+    	temp[k++]=a[j++];
+	}
+   	 
+	for(i=i1,j=0;i<=j2;i++,j++)
+	{
+    	a[i]=temp[j];
+	}    
 }
 
-void parallelMergeSort(int arr[], int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
 
-        #pragma omp parallel sections
-        {
-            #pragma omp section
-            {
-                parallelMergeSort(arr, low, mid);
-            }
+int main()
+{
+	int *a,n,i;
+	cout<<"\n enter total no of elements=>";
+	cin>>n;
+	a= new int[n];
 
-            #pragma omp section
-            {
-                parallelMergeSort(arr, mid + 1, high);
-            }
-        }
-        merge(arr, low, mid, high);
-    }
-}
-
-void mergeSort(int arr[], int low, int high) {
-    if (low < high) {
-        int mid = (low + high) / 2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
-    }
-}
-
-int main() {
-    int n = 10;
-    int arr[n];
-    double start_time, end_time;
-
-    // Create an array with numbers starting from n to 1.
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    // Measure Sequential Time
-    start_time = omp_get_wtime(); 
-    mergeSort(arr, 0, n - 1);
-    end_time = omp_get_wtime(); 
-    cout << "Time taken by sequential algorithm: " << end_time - start_time << " seconds\n";
-
-    // Reset the array
-    for(int i = 0, j = n; i < n; i++, j--) arr[i] = j;
-    
-    //Measure Parallel time
-    start_time = omp_get_wtime(); 
-    parallelMergeSort(arr, 0, n - 1);
-    end_time = omp_get_wtime(); 
-    cout << "Time taken by parallel algorithm: " << end_time - start_time << " seconds";
-    
-    return 0;
+	cout<<"\n enter elements=>";
+	for(i=0;i<n;i++)
+	{
+    	cin>>a[i];
+	}
+   //	 start=.......
+//#pragma omp…..
+	mergesort(a, 0, n-1);
+//          stop…….
+	cout<<"\n sorted array is=>";
+	for(i=0;i<n;i++)
+	{
+    	cout<<"\n"<<a[i];
+	}
+  	// Cout<<Stop-Start
+	return 0;
 }
